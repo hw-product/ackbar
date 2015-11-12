a1c60c13abb1a3de1ad0bd91338a6d651648a144
Chris Roberts <code@chrisroberts.org>
2015-11-12 11:31:41 -0800

    One more local test

--- sparkleformation/infrastructure.rb
+++ sparkleformation/infrastructure.rb
@@ -1,7 +1,7 @@
 SparkleFormation.new(:infrastructure).load(:base).overrides do
 
-  dynamic!(:s3_bucket, :update_storage)
+  dynamic!(:s3_bucket, :update2_storage)
 
-  outputs.bucket_name.value ref!(:update_storage_s3_bucket)
+  outputs.bucket_name.value ref!(:update2_storage_s3_bucket)
 
 end
