bd714e185be336e13bb23da0c14f78e20e45bccb
Chris Roberts <code@chrisroberts.org>
2015-11-12 08:17:06 -0800

    update name again

--- sparkleformation/infrastructure.rb
+++ sparkleformation/infrastructure.rb
@@ -1,7 +1,7 @@
 SparkleFormation.new(:infrastructure).load(:base).overrides do
 
-  dynamic!(:s3_bucket, :storage)
+  dynamic!(:s3_bucket, :update_storage)
 
-  outputs.bucket_name.value ref!(:storage_s3_bucket)
+  outputs.bucket_name.value ref!(:update_storage_s3_bucket)
 
 end
