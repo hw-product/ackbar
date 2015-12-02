15e9016280347eeddff4f8382a7d5ae807601d5d
Chris Roberts <code@chrisroberts.org>
2015-12-02 14:27:49 -0800

    update bucket

--- sparkleformation/infrastructure.rb
+++ sparkleformation/infrastructure.rb
@@ -1,7 +1,7 @@
 SparkleFormation.new(:infrastructure).load(:base).overrides do
 
-  dynamic!(:s3_bucket, :storage)
+  dynamic!(:s3_bucket, :storage_new)
 
-  outputs.bucket_name.value ref!(:storage_s3_bucket)
+  outputs.bucket_name.value ref!(:storage_new_s3_bucket)
 
 end
